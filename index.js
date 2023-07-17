9a5c15107a982a842e29602a3c00864a8ea35c21
Anita404 <writingonbookends@gmail.comm>
2023-07-17 17:12:10 +0200

    adds keyboard event listener

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log('hey')
+const letters = document.querySelectorAll(".scoreboard-letter");
+const loadingDiv = document.querySelectorAll(".info-bar");
+
+async function init() {
+
+    document.addEventListener("keydown", (event) => {
+        const action = event.key;
+
+        if (action == 'Enter') {
+            commit()
+        } else if (action == 'Backspace') {
+            backspace()
+        } else if (isLetter(action)) {
+            addLetter(action)
+        }
+    })
+}
+
+init()
+
+
+/* another approach but does not work for letters: 
+switch (action) {
+    case 'Enter':
+        commit();
+        break;
+    case 'Backspace':
+        backspace();
+        break;

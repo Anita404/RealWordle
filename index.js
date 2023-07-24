22ae3eb8198405136756ef07346eb41d220fcfdb
Anita404 <writingonbookends@gmail.comm>
2023-07-24 15:31:06 +0200

    Adds input handlers
    - Added a handler when a user tries to type a word(letter validation)
    -Added a handler when a user presses 'Backspace'
    -Added a handler when a user presses 'Enter'

--- index.js
+++ index.js
@@ -1,7 +1,41 @@
 const letters = document.querySelectorAll(".scoreboard-letter");
 const loadingDiv = document.querySelectorAll(".info-bar");
+const ANSWER_LENGTH = 5;
 
 async function init() {
+    let currentGuess = ''
+    let currentRow = 0;
+
+    function addLetter(letter) {
+        if (currentGuess.length < ANSWER_LENGTH) {
+            currentGuess += letter;
+        } else {
+            /// replace the last letter
+            currentGuess = currentGuess.substring(0, currentGuess.length - 1) + letter
+        }
+        letters[currentGuess.length - 1].innerText = letter
+    }
+
+    async function commit() {
+        if (currentGuess.length !== ANSWER_LENGTH) {
+            // do nothing 
+            return;
+        }
+
+        // TODO: validate the word
+
+        //TODO: do all the marking as "correct", "close" or "wrong"
+
+        // TODO: did they win or lose?
+
+        currentRow++;
+        currentGuess + "";
+    }
+
+    function backspace() {
+        currentGuess = currentGuess.substring(0, currentGuess.length - 1);
+        letters[currentGuess.length].innerText = "";
+    }
 
     document.addEventListener("keydown", (event) => {
         const action = event.key;
@@ -11,9 +45,15 @@ async function init() {
         } else if (action == 'Backspace') {
             backspace()
         } else if (isLetter(action)) {
-            addLetter(action)
+            addLetter(action);
         }
     })
+
+}
+
+
+function isLetter(letter) {
+    return /^[a-zA-Z]$/.test(letter);
 }
 
 init()
@@ -26,4 +66,4 @@ switch (action) {
         break;
     case 'Backspace':
         backspace();
-        break;
+        break; */
